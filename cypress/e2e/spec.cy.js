916547b2d56b0666c8817036509dcf3817b7e252
Bammy00 <64096018+Bammy00@users.noreply.github.com>
2023-08-03 00:51:55 +0100

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,32 +1,6 @@
-describe("Login to Facebook", () => {
-  const username = "your_facebook_username"; // Replace with actual username
-  const password = "your_facebook_password"; // Replace with actual password
-
-  it("Should log in to Facebook successfully", () => {
-    cy.visit("https://www.facebook.com");
-    cy.get("#email").type(username);
-    cy.get("#pass").type(password);
-    cy.get("#loginbutton").click();
-    cy.url().should("include", "facebook.com");
-  });
-});
-
-
-describe("Make a Post on Facebook", () => {
-  const postMessage = "I Kill Bugs!";
-
-  it("Should make a post on Facebook successfully", () => {
-    // Assume user is already logged in (You can use `beforeEach` to login before this test case)
-    cy.get('[data-testid="status-attachment-mentions-input"]').type(postMessage);
-    cy.get('[data-testid="react-composer-post-button"]').click();
-    cy.contains("Post").should("be.disabled"); // To confirm that the post was successful
-  });
-});
-
 
 describe.only("Search on Google", () => {
   const searchTerm = "best QA Engineering practices";
-
   beforeEach(() => {
     // Clear cookies and local storage before each test
     cy.clearCookies();
@@ -38,7 +12,7 @@ describe.only("Search on Google", () => {
     cy.get('#APjFqb').type(searchTerm + "{enter}"); // Press the "Enter" key after typing the search term
     // Wait for the search results to load
     cy.get('div[id="rcnt"]').should("be.visible");
-    // Optional: You can also check for the search input field to ensure the correct search term is typed
+    // Check for the search input field to ensure the correct search term is typed
     cy.get('#APjFqb').should("have.value", searchTerm);
   });
 });
