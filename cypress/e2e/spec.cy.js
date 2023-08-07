b9a68cb0a086128c9e5c5ae5cf09125490651ca1
Topset QA <ayobami@topset.app>
2023-08-07 16:35:46 +0100

    Completed

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
-
-describe.only("Search on Google", () => {
+describe("Search on Google", () => {
   const searchTerm = "best QA Engineering practices";
+
   beforeEach(() => {
     // Clear cookies and local storage before each test
     cy.clearCookies();
@@ -17,7 +17,35 @@ describe.only("Search on Google", () => {
   });
 });
 
+describe("Facebook Tests", () => {
+  const username = "your-facebook-username"; // Replace with your Facebook username
+  const password = "your-facebook-password"; // Replace with your Facebook password
+  const postContent = "I Kill Bugs!";
 
+  beforeEach(() => {
+    // Clear cookies and local storage before each test
+    cy.clearCookies();
+    cy.clearLocalStorage();
+  });
 
+  it("Should log in to Facebook successfully", () => {
+    cy.visit("https://www.facebook.com");
+    cy.get("#email").type(username);
+    cy.get("#pass").type(password);
+    cy.get('[data-testid="royal_login_button"]').click();
+    cy.url().should("not.include", "login"); // Ensure that login is successful and user is redirected
+  });
 
+  it("Should make a post 'I Kill Bugs!'", () => {
+    cy.visit("https://www.facebook.com");
+    cy.get("#email").type(username);
+    cy.get("#pass").type(password);
+    cy.get('[data-testid="royal_login_button"]').click();
+    cy.url().should("not.include", "login"); // Ensure that login is successful and user is redirected
 
+    // Make a post "I Kill Bugs!"
+    cy.get('[data-testid="status-attachment-mentions-input"]').type(postContent);
+    cy.get('[data-testid="react-composer-post-button"]').click();
+    cy.get('[data-testid="status-attachment-mentions-input"]').should("be.empty"); // Ensure that the input field is empty after posting
+  });
+});
